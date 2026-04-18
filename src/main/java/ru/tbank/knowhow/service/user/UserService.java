1759a1e87928b1cd5b2a262407b12e2626581763
Yanin Vyacheslav <slava.vy.2006@gmail.com>
2026-04-18 11:23:09 +0300

    поменял import @Transactional

--- src/main/java/ru/tbank/knowhow/service/user/UserService.java
+++ src/main/java/ru/tbank/knowhow/service/user/UserService.java
@@ -1,12 +1,12 @@
 package ru.tbank.knowhow.service.user;
 
 import jakarta.persistence.EntityNotFoundException;
-import jakarta.transaction.Transactional;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import ru.tbank.knowhow.ecxeption.RegistrationException;
 import ru.tbank.knowhow.model.Balance;
 import ru.tbank.knowhow.model.Role;
