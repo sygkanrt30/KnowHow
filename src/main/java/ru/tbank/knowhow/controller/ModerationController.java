42e08506091f1f9e2629ce9a11f01598a0c7d223
anastasia <bolobanav6203214yandex.ru>
2026-04-30 11:31:13 +0300

    исправленный эндпоинт отклонения модератором

--- src/main/java/ru/tbank/knowhow/controller/ModerationController.java
+++ src/main/java/ru/tbank/knowhow/controller/ModerationController.java
@@ -1,14 +1,17 @@
 package ru.tbank.knowhow.controller;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import ru.tbank.knowhow.model.dto.request.ModerationRejectRequest;
 import ru.tbank.knowhow.service.moder.ModerationService;
 import java.util.Map;
 
+@Validated
 @RestController
 @RequestMapping("${server.base-url.course}/moderation")
 @RequiredArgsConstructor
@@ -29,7 +32,7 @@ public class ModerationController {
     public ResponseEntity<String> rejectCourse(
             @PathVariable Long id,
             @AuthenticationPrincipal UserDetails userDetails,
-            @RequestBody ModerationRejectRequest request) {
+            @Valid @RequestBody ModerationRejectRequest request) {
 
         moderationService.rejectCourse(id, userDetails.getUsername(), request.getReason());
         return ResponseEntity.ok("Курс отклонён");
--- src/main/java/ru/tbank/knowhow/model/dto/request/ModerationRejectRequest.java
+++ src/main/java/ru/tbank/knowhow/model/dto/request/ModerationRejectRequest.java
@@ -1,8 +1,10 @@
 package ru.tbank.knowhow.model.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Data
 public class ModerationRejectRequest {
+    @NotBlank(message = "Причина отклонения не может быть пустой")
     private String reason;
 }
